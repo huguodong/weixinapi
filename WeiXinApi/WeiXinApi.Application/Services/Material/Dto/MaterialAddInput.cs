1842680bb6496750d5454ab6bf6a38679a9dd6b7
huguodong\Shiny <hugudong@zxzyjs.com>
2022-06-06 14:12:32 +0800

    素材管理

--- WeiXinApi/WeiXinApi.Application/Services/Material/Dto/MaterialAddInput.cs
+++ WeiXinApi/WeiXinApi.Application/Services/Material/Dto/MaterialAddInput.cs
@@ -16,4 +16,50 @@ namespace WeiXinApi.Application.Services
         [Required(ErrorMessage = "文件不存在")]
         public IFormFile File { get; set; }
     }
+
+    public class ForeverAddInput
+    {
+        /// <summary>
+        /// 文件类型
+        /// </summary>
+        [Required(ErrorMessage = "文件类型不能为空")]
+        public UploadForeverMediaType? UploadForeverMediaType { get; set; }
+
+        /// <summary>
+        /// 文件
+        /// </summary>
+        [Required(ErrorMessage = "文件不存在")]
+        public IFormFile File { get; set; }
+
+        /// <summary>
+        /// 标题
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Introduction { get; set; }
+    }
+
+
+
+    public class ForeverPageInput
+    {
+        /// <summary>
+        /// 媒体类型
+        /// </summary>
+        public UploadMediaFileType UploadMediaFileType { get; set; }
+
+        /// <summary>
+        /// 从全部素材的该偏移位置开始返回，0表示从第一个素材 返回
+        /// </summary>
+        public int Offset { get; set; }
+
+        /// <summary>
+        /// 返回素材的数量，取值在1到20之间
+        /// </summary>
+        public int Count { get; set; } = 20;
+    }
+
 }
--- WeiXinApi/WeiXinApi.Application/Services/Material/MaterialService.cs
+++ WeiXinApi/WeiXinApi.Application/Services/Material/MaterialService.cs
@@ -17,6 +17,123 @@
             return uploadResult.media_id;
         }
 
+
+        /// <summary>
+        /// 上传永久素材
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPost("/upload/forever")]
+        public async Task<dynamic> UploadForever([FromForm] ForeverAddInput input)
+        {
+
+            var file = await GetPath(input.File);
+            var uploadResult = await MediaApi.UploadForeverMediaAsync(AppId, file, input.UploadForeverMediaType.Value);
+            return uploadResult;
+        }
+
+        /// <summary>
+        /// 上传永久视频素材
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPost("/upload/video")]
+        public async Task<dynamic> UploadForeverVideo([FromForm] ForeverAddInput input)
+        {
+
+            var file = await GetPath(input.File);
+            var uploadResult = await MediaApi.UploadForeverVideoAsync(AppId, file, input.Title, input.Introduction);
+            return uploadResult;
+        }
+
+
+
+        /// <summary>
+        /// 上传图文消息内的图片获取URL
+        /// </summary>
+        /// <param name="formFile"></param>
+        /// <returns></returns>
+        [HttpPost("/upload/image")]
+        public async Task<dynamic> UploadImage(IFormFile formFile)
+        {
+            if (formFile != null)
+            {
+                var file = await GetPath(formFile);
+                var uploadResult = await MediaApi.UploadImgAsync(AppId, file);
+                return uploadResult;
+            }
+            else
+            {
+                throw Oops.Oh($"文件不存在");
+            }
+        }
+
+
+        /// <summary>
+        /// 删除素材
+        /// </summary>
+        /// <param name="mediaId"></param>
+        /// <returns></returns>
+        [HttpPost("/upload/delete/{mediaId}")]
+        public async Task<dynamic> Delete(string mediaId)
+        {
+            var result = await MediaApi.DeleteForeverMediaAsync(AppId, mediaId);
+            return result;
+        }
+
+        /// <summary>
+        /// 获取临时素材
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpGet("/material/temp/get")]
+        public async Task<dynamic> GetTemp(string mediaId)
+        {
+            var result = await MediaApi.GetAsync(AppId, mediaId, App.HostEnvironment.ContentRootPath);
+            return result;
+        }
+
+        /// <summary>
+        /// 素材总数
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpGet("/material/total")]
+        public async Task<dynamic> Total()
+        {
+            var result = await MediaApi.GetMediaCountAsync(AppId);
+            return result;
+        }
+
+
+
+        /// <summary>
+        /// 图文素材列表
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpGet("/material/imagepage")]
+        public async Task<dynamic> ImagePage([FromQuery] ForeverPageInput input)
+        {
+            var result = await MediaApi.GetNewsMediaListAsync(AppId, input.Offset, input.Count);
+            return result;
+        }
+
+        /// <summary>
+        /// 素材列表
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpGet("/material/page")]
+        public async Task<dynamic> Page([FromQuery] ForeverPageInput input)
+        {
+            var result = await MediaApi.GetOthersMediaListAsync(AppId, input.UploadMediaFileType, input.Offset, input.Count);
+            return result;
+        }
+
+
+
+
         /// <summary>
         /// 保存文件到本地
         /// </summary>
@@ -39,5 +156,8 @@
             }
             return filePath;
         }
+
+
+
     }
 }
